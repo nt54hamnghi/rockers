610c10c30e548a275e4ec987b8ea4625256829b6
nt54hamnghi <hamnghi250699@gmail.com>
2026-05-02 10:55:06 -0400

    feat: set up UTS namespace for hostname

--- src/cli/pull.rs
+++ src/cli/pull.rs
@@ -12,7 +12,7 @@ use tokio::task::JoinSet;
 use crate::cli::PullArgs;
 use crate::registry::{ImageManifest, RegistryClient};
 
-const TARGET: &str = "./tmp/rootfs-new3";
+const TARGET: &str = "./tmp/rootfs";
 
 impl PullArgs {
     pub async fn run(&self) -> anyhow::Result<()> {
--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,8 +1,40 @@
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+
+use nix::sched::CloneFlags;
+
 use crate::cli::RunArgs;
 
 impl RunArgs {
     pub fn run(&self) -> anyhow::Result<()> {
-        println!("{:?}", self.command);
+        let (bin, args) = self
+            .command
+            .split_first()
+            .expect("run command is required by clap");
+
+        println!("Running: {:?}, with: {:?}", bin, args);
+
+        let mut cmd = Command::new(bin);
+        cmd.args(args);
+
+        // SAFETY: `pre_exec` runs in the child after `fork` and before `exec`
+        // This closure only performs `unshare` and does not use allocate memory
+        // access environment variables with std::env, or acquire a mutex.
+        unsafe {
+            cmd.pre_exec(|| {
+                nix::sched::unshare(CloneFlags::CLONE_NEWUTS)?;
+                Ok(())
+            });
+        }
+
+        let mut child = cmd.spawn()?;
+        let status = child.wait().expect("command wasn't running");
+
+        if !status.success() {
+            let exit_code = status.code().unwrap_or(-1);
+            anyhow::bail!("process exited with code: {}", exit_code);
+        }
+
         Ok(())
     }
 }
